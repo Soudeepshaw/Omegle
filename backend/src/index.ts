adf11a2e872738b12ce8e41a3cc684bfbb1548f3
Soudeep shaw <119213922+Soudeepshaw@users.noreply.github.com>
2024-07-24 00:14:05 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CORS_ORIGIN || "*",
+    origin: "https://omegle-fr.onrender.com",
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true
